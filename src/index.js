ca6d88ab1f8e14576e49c3d241acfce75189dbee
RuYi-Chen <69340316+gras-chen@users.noreply.github.com>
2021-08-28 15:48:20 +0800

    [Add] 新增檔案

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// Step 1: Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Step 2: Create a react component
+const App = function(){
+    return (
+        <div>Hi there!</div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Step 3: Take the react component and show it on the screen
+ReactDOM.render( <App />, document.querySelector('#root') );
